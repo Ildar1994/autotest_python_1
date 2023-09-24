34a82127f16e3803d7bd0919ad233dab83524ee0
Ildar1994 <86912735+Ildar1994@users.noreply.github.com>
2023-09-24 16:27:36 +0300

    Удалил scope=function conftest.py

--- conftest.py
+++ conftest.py
@@ -2,7 +2,7 @@ import pytest
 from selene import be, have, browser
 
 
-@pytest.fixture(scope='function')
+@pytest.fixture()
 def browser_settings():
     browser.config.window_width = 1920
     browser.config.window_height = 1080
